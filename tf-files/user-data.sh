2acbd24a5b5f854f9a427769c4417ddcc63d19f4
Ömer Faruk Demirözü <81289191+MiranaSGit@users.noreply.github.com>
2021-10-11 17:14:45 +0300

    Update user-data.sh

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="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"
+TOKEN="ghp_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 cd /home/ec2-user && git clone https://$TOKEN@github.com/MiranaSGit/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
